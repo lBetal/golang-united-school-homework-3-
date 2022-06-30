62353d2be6608afe4e7107b5406c7d67ed436642
lBetal <mehdiev.t@gmail.com>
2022-06-30 13:03:24 +0600

    task2

--- task02-slice.go
+++ task02-slice.go
@@ -1,13 +1,9 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	for i := 0; i < len(input); i++ {
-		for j := 0; j < len(input); j++ {
-			if input[i] > input[j] {
-				input[i], input[j] = input[j], input[i]
-			}
-		}
+	for i := len(input) - 1; i >= 0; i-- {
+		result = append(result, input[i])
 	}
-	result = append(result, input...)
+
 	return
 }
